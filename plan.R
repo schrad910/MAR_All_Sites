6de55e6b5edd476471d31ee66f249e19012f8426
nschrad910 <nicoleschrad@gmail.com>
2021-11-03 07:48:12 -0700

    redundant deletions

--- plan.R
+++
@@ -1,6 +0,0 @@
-Plan<- list( SoilTypes= c("high sand", "low sand"),
-             Location= c("5b top", "5c top"),
-             Treatments = c("WC", "alfalfa", "biochar", "Almond Shell", "negative"))
-
-
-             
