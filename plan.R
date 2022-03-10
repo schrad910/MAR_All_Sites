aa0c025461f755c0cce7c03aaa5e9eedde3db0de
Nicole Schrad <nicoleschrad@eduroam-169-233-186-212.ucsc.edu>
2022-03-10 10:44:14 -0800

    idk transfering between two computers

---
+++ plan.R
@@ -0,0 +1,6 @@
+Plan<- list( SoilTypes= c("high sand", "low sand"),
+             Location= c("5b top", "5c top"),
+             Treatments = c("WC", "alfalfa", "biochar", "Almond Shell", "negative"))
+
+
+             
